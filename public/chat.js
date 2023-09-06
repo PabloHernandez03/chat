b3e03510478e92a29d3670acd669e39b11864729
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2023-09-05 19:50:02 -0600

    second commit

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://chat-ayf5.onrender.com');
 
 // Query DOM
 var message = document.getElementById('message'),
